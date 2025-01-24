f287baf056c1b9dd643a49af620d842d54bc05eb
Tejeshwar Singh <87794808+Tejeshwar006@users.noreply.github.com>
2025-01-24 16:26:47 +0000

    commit

---
+++ codes/variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+    int x;//declaration
+    x = 5;//initialization
+    int y = 20;
+
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+
+    //integer - int
+    //floating point - float or double (double is more precise)
+    //character - char (single character) or string (multiple characters)
+    //boolean - bool (true or false) 
+    
+
+    return 0;
+}
