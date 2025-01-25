208815a4f4c1447f3bae4dd85e62e63daefe4ec9
Tejeshwar Singh <87794808+Tejeshwar006@users.noreply.github.com>
2025-01-25 09:10:22 +0000

    commit

--- codes/typedef.cpp
+++ codes/typedef.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
 
-typedef std::string str;
+// typedef std::string str;
+//typedef can be used as "using" keyword
+//using str = std::string;
+using str= std::string;
+
 int main(){
 
     str sentence = "hello guys";
